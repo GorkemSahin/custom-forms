f9de67bc3fcc585ea03f4a9a43ae0460462c87ce
GorkemSahin <gorkemsahin.1997@gmail.com>
2025-03-12 22:03:45 +0900

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Stack,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { Form } from "./components/form/Form";
+import { FormConfigInput } from "./components/formConfig/FormConfigInput";
+import { FORM_TABS } from "./utils/const";
+import { FormConfig, FormTab } from "./utils/types";
 
-function App() {
+export function App() {
+  const [activeTab, setActiveTab] = useState<FormTab>("CONFIG");
+
+  const [formConfig, setFormConfig] = useState<FormConfig | null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack direction="column" spacing={2} className="app">
+      <Typography variant="h5" component="h1">
+        Custom Form Generator
+      </Typography>
+      <Tabs
+        value={activeTab}
+        onChange={(_, newValue) => setActiveTab(newValue)}
+      >
+        {FORM_TABS.map((tab) => (
+          <Tab {...tab} key={tab.value} />
+        ))}
+      </Tabs>
+
+      <Box
+        style={{
+          flex: 1,
+          overflow: "hidden",
+        }}
+      >
+        {activeTab === "CONFIG" && <FormConfigInput onApply={setFormConfig} />}
+
+        {activeTab === "FORM" && formConfig ? (
+          <Form formConfig={formConfig} />
+        ) : (
+          <Alert severity="warning">
+            <AlertTitle>
+              No valid form configuration has been provided.
+            </AlertTitle>
+            Please enter and apply a valid form configuration using the FORM
+            CONFIGURATION tab.
+          </Alert>
+        )}
+      </Box>
+    </Stack>
   );
 }
-
-export default App;
